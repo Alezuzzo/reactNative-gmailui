ef6311e471bc0116136cb4c9b3101f5ebaf1f99c
Thalles Alexsander <thallesander@hotmail.com>
2024-03-04 13:02:32 -0300

    creating project, configuring NativeWind and gesturehandler

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+
+
+export default function Home(){
+    return (
+    <View className="flex-1 bg-red-600">
+        <Text>Home</Text>
+    </View>
+    )
+}
